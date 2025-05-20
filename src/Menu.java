18c4cbbd1d82cd545edbb5cffcdb3b843a76a4b4
caarat156 <amaylinda@student.ciputra.ac.id>
2025-05-20 14:01:35 +0700

    opening

--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,33 @@
 import java.util.*;
 public class Menu {
-    
+    Scanner scan= new Scanner(System.in);
+
+    public void welcome(){
+        System.out.println("""
+                Welcome to Preet
+                \033[1m"I was once a seed — small, still, and buried.
+                But something inside me wanted the light."\033[0m
+
+                You are not planting a tree.
+                \033[1mYou are the tree.\033[0m
+
+                Each day, your actions water, movement, rest are sunlight and rain.
+
+                You stretch a little taller.
+                Your roots grow deeper.
+                Your leaves grow stronger.
+
+                A gentle companion watches over you,
+                cheering when you rise,
+                comforting when you fall.
+
+                There is no final stage.
+                Only seasons.
+                Some fast, some slow.
+                All part of your becoming.
+
+                \033[0;37m\033[01You are the tree.
+                And you are still growing.\033[m
+                """);
+    }
 }
