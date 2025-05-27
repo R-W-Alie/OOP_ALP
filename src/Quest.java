cb8db7b72b16f36d7edd278f1d7896185596bd04
caarat156 <amaylinda@student.ciputra.ac.id>
2025-05-27 10:41:42 +0700

    class quest

--- src/Quest.java
+++ src/Quest.java
@@ -1,3 +1,19 @@
-public class Quest {
-    
+public abstract class Quest {
+    protected String name;
+    protected String description;
+
+    public Quest(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public abstract void complete(User user);
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
