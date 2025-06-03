171f2eccfc89f4b432b0edf25d2dc2af954eb6a5
R-W-Alie <rwijayaalie@student.ciputra.ac.id>
2025-06-03 14:24:45 +0700

    gtw

--- src/Menu.java
+++ src/Menu.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 public class Menu {
-    private static final String USER_DATA_DIR = "user_data/";
     private Scanner s = new Scanner(System.in);
     private String currentUser = null;
 
@@ -179,48 +178,3 @@ public class Menu {
         // ini dia display quest yang bakal dilakukan
     }
 }
-
-// public void menu(){
-// System.out.println("""
-// ==========================
-// 🌳Preet🌳
-// ==========================
-// Welcome to Preet! This is a place where you can grow and learn.
-// You can choose to start your journey or log in if you already have an
-// account.
-// 1. Start
-// 2. Log in
-// 3. Exit
-// """);
-// int pilih = s.nextInt();
-// switch (pilih) {
-// case 1 -> start();
-// case 2 -> login();
-// case 3 -> {
-// System.out.println("Exiting the program...");
-// System.exit(0);
-// }
-// default -> System.out.println("Invalid choice. Please try again.");
-// }
-// }
-
-// public void start(){
-// System.out.println("Hi! Welcome to Preet. Let's get started.");
-// System.out.println("Please enter your username:");
-// String username = s.next();
-// System.out.println("Please enter your password:");
-// String password = scan.next();
-// //pake hashmap?
-// }
-
-// public void login(){
-// System.out.println("Please enter your username:");
-// String username = s.next();
-// System.out.println("Please enter your password:");
-// String password = s.next();
-// // Here you would typically check the username and password against a
-// database or a list of users
-// // For now, we'll just print them out
-// System.out.println("Username: " + username);
-// System.out.println("Password: " + password);
-// }
